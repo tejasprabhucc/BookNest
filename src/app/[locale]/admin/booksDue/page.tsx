5040a056437c0488256efc1d427ad1328c2f46fc
tejas <tejas.prabhu@codecraft.co.in>
2024-09-23 14:36:49 +0530

    Completed localization for login and signup page

---
+++ src/app/[locale]/admin/booksDue/page.tsx
@@ -0,0 +1,87 @@
+// import React from "react";
+// import { fetchTransactions } from "@/src/lib/actions";
+// import {
+//     FilterOptions,
+//   IPagedResponse,
+//   IPaginationOptions,
+//   ITransaction,
+//   SortOptions,
+// } from "@/src/lib/definitions";
+// import PaginationControl from "@/src/components/controls/pagination";
+// import Search from "@/src/components/navbar/search";
+// import TransactionsTable from "@/src/components/dashboard/transactions-table"; // Ensure you have this component
+// import { ITransactionDetails } from "@/src/repositories/transaction.repository";
+
+// const BooksDue = async ({
+//   searchParams,
+// }: {
+//   searchParams?: {
+//     query?: string;
+//     page?: string;
+//   };
+// }) => {
+//   let currentPage = Number(searchParams?.page) || 1;
+//   const searchQuery = searchParams?.query || undefined;
+//   const limit = 8;
+//   let transactions: ITransactionDetails[] | undefined = [];
+//   let paginationOptions: IPaginationOptions = {
+//     offset: 0,
+//     limit: 8,
+//     total: 0,
+//   };
+//   let sortOptions: SortOptions<ITransaction> = {
+//     sortBy: "id",
+//     sortOrder: "desc",
+//   };
+//   let filterOptions: FilterOptions<ITransaction> = {
+//     bookStatus: ,
+//   };
+//   let errorMessage: string | null = null;
+
+//   try {
+//     const fetchTransactionsResult = (await fetchTransactions(
+//       {
+//         search: searchQuery,
+//         offset: currentPage * 8 - 8,
+//         limit: limit,
+//       },
+//       sortOptions
+//     )) as IPagedResponse<ITransactionDetails>;
+
+//     if (!fetchTransactionsResult || !fetchTransactionsResult.items.length) {
+//       transactions = [];
+//       paginationOptions = { ...paginationOptions, total: 0 };
+//     } else {
+//       paginationOptions = fetchTransactionsResult.pagination;
+//       transactions = fetchTransactionsResult.items;
+//     }
+//   } catch (error) {
+//     errorMessage = "No transactions found.";
+//   }
+
+//   return (
+//     <main className="flex flex-1 flex-col gap-2 overflow-y-auto p-4 px-8">
+//       <h1 className="text-3xl mb-3 font-serif lg:text-5xl">Transactions</h1>
+//       <div className="flex items-center justify-between">
+//         <Search placeholder="Enter a keyword..." />
+//       </div>
+//       {errorMessage ? (
+//         <p>{errorMessage}</p>
+//       ) : transactions.length > 0 ? (
+//         <TransactionsTable transactions={transactions} actions={true} />
+//       ) : (
+//         <p>No transactions found.</p>
+//       )}
+//       <div className="flex justify-center align-middle m-auto my-1">
+//         {transactions.length > 0 ? (
+//           <PaginationControl
+//             currentPage={currentPage}
+//             options={paginationOptions}
+//           />
+//         ) : null}
+//       </div>
+//     </main>
+//   );
+// };
+
+// export default BooksDue;
--- src/app/[locale]/signup/page.tsx
+++ src/app/[locale]/signup/page.tsx
@@ -10,16 +10,16 @@ import React from "react";
 import Link from "next/link";
 import SignupForm from "@/src/components/signup-form";
 import GoogleAuthButton from "@/src/components/googleAuthButton";
+import { useTranslations } from "next-intl";
 
 const Signup = () => {
+  const t = useTranslations("Signup");
   return (
     <div className="flex min-h-screen items-center justify-center bg-background px-4  sm:px-6 lg:px-8">
       <Card className="w-full max-w-md shadow-lg">
         <CardHeader className="space-y-1 text-center">
-          <CardTitle className="text-2xl font-bold">
-            Create an account
-          </CardTitle>
-          <CardDescription>Enter your details to register.</CardDescription>
+          <CardTitle className="text-2xl font-bold">{t("title")}</CardTitle>
+          <CardDescription> {t("description")}</CardDescription>
         </CardHeader>
         <CardContent className="space-y-3">
           <SignupForm />
@@ -27,9 +27,9 @@ const Signup = () => {
           <GoogleAuthButton />
           <div>
             <p>
-              Already have an account?{" "}
+              {t("help")}{" "}
               <Link className="font-bold" href={"/login"}>
-                Login
+                {t("link")}
               </Link>{" "}
             </p>
           </div>
