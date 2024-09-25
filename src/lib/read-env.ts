6da9ef67a77b8ee0a59d61b1c31a0987ca926446
tejasprabhucc <163384136+tejasprabhucc@users.noreply.github.com>
2024-09-25 15:48:26 +0530

    Localization (#11)
    * Resolved build errors

--- src/lib/read-env.ts
+++ src/lib/read-env.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 interface AppEnv {
-  // DATABASE_URL: string;
+  DATABASE_URL: string;
   AUTH_GOOGLE_ID: string;
   AUTH_SECRET: string;
   POSTGRES_URL: string;
@@ -11,9 +11,9 @@ interface AppEnv {
 }
 
 const getAppEnvs = (): AppEnv => {
-  // if (!process.env.DATABASE_URL) {
-  //   throw new Error("DATABASE_URL is not defined in the environment variables");
-  // }
+  if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not defined in the environment variables");
+  }
 
   if (!process.env.AUTH_GOOGLE_ID) {
     throw new Error(
@@ -33,7 +33,7 @@ const getAppEnvs = (): AppEnv => {
   }
 
   return {
-    // DATABASE_URL: process.env.DATABASE_URL,
+    DATABASE_URL: process.env.DATABASE_URL,
     AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
     AUTH_SECRET: process.env.AUTH_SECRET,
     POSTGRES_URL: process.env.POSTGRES_URL,
